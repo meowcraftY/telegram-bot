bc2b85f0e7f8cb24457671f3fb7ef402c112a750
meowcraftY <79714367+meowcraftY@users.noreply.github.com>
2021-03-11 17:54:09 +0700

    Create tele.js

---
+++ tele.js
@@ -0,0 +1,32 @@
+const { color, bgcolor } = require('./lib/func/color')
+const setting = require('./config.json')
+const { menu } = require('./lib/src/help')
+const { getBuffer, banner} = require('./lib/func/buffer')
+
+const Telebot = require('telebot')
+const axios = require('axios')
+const { token, meowkey } = setting
+
+async function stars() {
+     const client = new Telebot({token: token})
+     
+     bot.on(["/start","/menu"], async (msg, args) => {
+        var kybd = client.keyboard([
+            ["/menu","/info"],
+        ], {resize: true})
+        await client.sendPhoto(msg.chat.id, './lib/src/icon.jpg', {caption: `🤖 MEOW BOT 🤖
+
+◪ Hai ${msg.from.username}!
+
+⎔ /info
+⎔ /coming
+
+◆ SOSIAL MEDA
+♡ IG: @meowcraft_
+♡ YT: meowcraft
+`
+})
+       return client.sendMessage(msg.chat.id, `Follow sosmed admin Ya!`, {kybd})
+     })
+}
+stars()
