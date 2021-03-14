97cae75716e8e75c2345d77c0f27b1c8c17d59be
meowcraftY <79714367+meowcraftY@users.noreply.github.com>
2021-03-14 13:17:46 +0700

    Update tele.js

--- tele.js
+++ tele.js
@@ -76,7 +76,7 @@ async function starts() {
      })
      client.on('/info', async (msg, args) => {
         isCmd("/info", msg.from.username)
-        client.sendPhoto(msg.chat.id, "./lib/src/icon.jpeg", {caption: "◪ INFO\n\n• BotName: Meow-Bot\n• Owner: @MeowCraftG\n• Prefix: /\nStatus: Soon\n\n⬤ SOSMED\n\n⎔ YT: MeowCraft\n⎔ IG: @meowcraft_", replyToMessage: msg.message_id})
+        client.sendPhoto(msg.chat.id, "./lib/src/icon.jpeg", {caption: "◪ INFO\n\n• BotName: Meow-Bot\n• Owner: @MeowCraftG\n• Prefix: /\n• Status: Soon\n\n⬤ SOSMED\n\n⎔ YT: MeowCraft\n⎔ IG: @meowcraft_", replyToMessage: msg.message_id})
      })
      client.on(/^\/return ([\s\S]+)/, async (msg, args) => {
         isCmd("/return", msg.from.username)
