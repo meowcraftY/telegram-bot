ca9a4b2e178ee2017101b28469f88e3a3e4f365e
meowcraftY <meowmc091@gmail.com>
2023-03-17 07:00:30 +0000

    update.

--- tele.js
+++ tele.js
@@ -12,6 +12,7 @@ const { token, openai_key } = setting
 const configuration = new Configuration({apiKey: "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"})
 const openai = new OpenAIApi(configuration)
 
+global.db = []
 
      console.log(banner.string)
      console.log(color("[SERVER]", "orange"), color("Server Started!"))
@@ -29,7 +30,17 @@ const openai = new OpenAIApi(configuration)
 
      client.on('text', async (msg) => {
          if (msg.text.startsWith('/start') || msg.text.startsWith('/help') || msg.text.startsWith('/menu') || msg.text.startsWith('/genimg')) return
-         chatgpt = await openai.createChatCompletion({model:'gpt-3.5-turbo',messages: [{role:'user',content: msg.text}]})
+         posisiDataChat = global.db.findIndex((d) => d.chat_id == msg.chat.id)
+         if (posisiDataChat == -1) {
+             pushData = global.db.push({chat_id: msg.chat.id, messages: []})
+             posisiDataChat = pushData - 1
+         }
+         chatgpt = await openai.createChatCompletion({model:'gpt-3.5-turbo',messages: [{role:'system',content: "You are Meow Assistant, an assitant that uses openai gpt 3.5 model, answer as concisely as possible."}, ...global.db[posisiDataChat].messages, {role: "user", content: msg.text}]})
+         if (global.db[posisiDataChat].messages.length === 12) {
+             global.db[posisiDataChat].messages.splice(0, 2)
+         }
+         global.db[posisiDataChat].messages.push({...chatgpt.data.choices[0].message},{"role":"user", "content":msg.text})
+         console.log(global.db)
          return client.sendMessage(msg.chat.id,chatgpt.data.choices[0].message.content, {replyToMessage: msg.message_id})
      })
 
